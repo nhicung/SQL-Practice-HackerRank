1f8c837e857c8c8e82353255b64184559ec78c61
Nhi Cung <nhcung@knox.edu>
2020-02-05 23:13:05 -0600

    message

--- Japan'sCities.sql
+++ Japan'sCities.sql
@@ -5,3 +5,11 @@ The COUNTRYCODE for Japan is JPN.
 select name 
 from city 
 where countrycode = 'JPN';
+
+/*
+2. Query the sum of the populations for all Japanese cities in CITY. 
+The COUNTRYCODE for Japan is JPN.
+*/
+select sum(population)
+from city 
+where countrycode = "jpn"
