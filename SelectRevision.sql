011b15cc9590930c0f36a970bd5700b9be34661e
Nhi Cung <nhcung@knox.edu>
2020-02-14 21:25:53 -0600

    update

--- SelectRevision.sql
+++ SelectRevision.sql
@@ -6,3 +6,12 @@ The CountryCode for America is USA.
 select *
 from CITY
 where countrycode = 'USA' and population > 100000;
+
+/*
+2. Query the names of all American cities in CITY with populations larger than 120000. 
+The CountryCode for America is USA.
+*/
+
+select NAME
+from CITY
+where CountryCode = 'USA' and population > 120000;
